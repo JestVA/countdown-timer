65abebdf7d02b23dcd66266f365421814ddc5e0c
Dorin Dumitrascuta <d.dumitrascuta@gmail.com>
2019-01-07 01:39:16 +0000

    basic countdown function complete

--- timer.js
+++ timer.js
@@ -1,5 +1,19 @@
+let countDown;
+
 const timer = seconds => {
     const now = Date.now();
     const then = now + seconds * 1000;
-    console.log(now, then)
+    displayTimeLeft(seconds)
+    countDown = setInterval(() => {
+        const secondsLeft = Math.round((then - Date.now()) / 1000);
+        if(secondsLeft < 0) {
+            clearInterval(countDown)
+            return;
+        }
+        displayTimeLeft(secondsLeft)
+    }, 1000);
+}
+
+const displayTimeLeft = seconds => {
+    console.log(seconds);
 }
